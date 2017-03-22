b3666a323dace05da4df61e60665b5d4f4737b03
harsha <vemulapatiharsha@gmail.com>
2017-03-22 22:45:28 +0530

    just funcs without download

--- dict.py
+++ dict.py
@@ -10,14 +10,14 @@ def getStats(fileName):
 	temp['time'] = os.stats(fileName).st_mtime
 	temp['size'] = os.stats(fileName).st_size
 	temp['name'] = fileName
-	if os.path.isdir(fileName):
+	if os.isdir(fileName):
 		temp['type'] = 'dir'
 	else:
 		temp['type'] = 'file'
 
 def longlist(path,conn):
 	result = []
-	for file in os.path.listdir(path):
+	for file in os.listdir(path):
 		temp = getStats(file)
 		result.append(temp)
 	resultJson = json.dumps(result)
@@ -26,7 +26,7 @@ def longlist(path,conn):
 
 def shortlist(a,b,path,conn):
 	result = []
-	for file in os.path.listdir(path):
+	for file in os.listdir(path):
 		temp = getStats(file)
 		if datetime.datetime.fortimestamp(temp['time']) >= a and datetime.datetime.fortimestamp(temp['time']) <= b:
 			result.append(temp)
@@ -36,23 +36,33 @@ def shortlist(a,b,path,conn):
 
 def regex(regExp,path,conn):
 	result = []
-	for file in os.path.listdir(path):
+	for file in os.listdir(path):
 		if re.search(regExp,file):
 			temp = getStats(file)
 			result.append(temp)
-	
+	resultJson = json.dumps(result)
+	conn.send(resultJson)
 	pass
 
 def checksum(fileName):
-	hashlib.md5(open(fileName,'rb').read()).hexdigest()
+	return hashlib.md5(open(fileName,'rb').read()).hexdigest()
 
 def verify(fileName,conn):
 	value = checksum(fileName)
 	for file in os.listdir(path):
 		if file == fileName:
 			temp = getStats(file)
-			print value,temp['time']
-def checkall(conn):
+			result = [{'value':value,'time':temp['time']}]
+	resultJson = json.dumps(result)
+	conn.send(resultJson)
+
+def checkall(conn,path):
+	for file in os.listdir(path):
+		temp = getStats(file)
+		value = checksum(file)
+		result.append({'name':file,'time':temp['time'],'value':value})
+	resultJson = json.dumps(result)
+	conn.send(resultJson)
 	pass
 
 
@@ -62,11 +72,12 @@ def parseQuery(query,conn):
 		if command[1] == "longlist":
 			longlist('.',conn)
 		elif command[1] == "shortlist":
-			shortlist(command[2],command[3],'.')
+			shortlist(command[2],command[3],'.',conn)
 		elif command[1] == "regex":
-			regex(command[2],'.')
+			regex(command[2],'.',conn)
 	elif command[0] == "hash":
 		if command[1] == "verify":
-			verify(command[2])
+			verify(command[2],conn)
 		elif command[1] == "checkall":
-			checkall()
+			checkall(conn,path)
+
